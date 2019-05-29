fc7b6feb573b53023c10436160616e87b944597a
Fabian Eidens <feidens@mail.uni-paderborn.de>
2019-05-29 12:04:44 +0200

    get rid of eccelerate

--- src/main/java/de/upb/crypto/craco/sig/sps/eq/SPSEQPublicParametersGen.java
+++ src/main/java/de/upb/crypto/craco/sig/sps/eq/SPSEQPublicParametersGen.java
@@ -4,7 +4,6 @@ import de.upb.crypto.math.factory.BilinearGroup;
 import de.upb.crypto.math.factory.BilinearGroupFactory;
 import de.upb.crypto.math.interfaces.mappings.BilinearMap;
 import de.upb.crypto.math.pairings.bn.BarretoNaehrigProvider;
-import de.upb.crypto.math.pairings.eccelerate.ECCelerateBilinearGroupProvider;
 
 import java.util.Arrays;
 
@@ -22,7 +21,7 @@ public class SPSEQPublicParametersGen {
         fac.setDebugMode(debugMode);
         fac.setRequirements(BilinearGroup.Type.TYPE_3);
         fac.registerProvider(Arrays.asList(
-                new ECCelerateBilinearGroupProvider(),
+                // new ECCelerateBilinearGroupProvider(), //not yet publicly available
                 // new BarretoNaehrigNativeProvider(), //not yet publicly available
                 new BarretoNaehrigProvider()));
         BilinearGroup group = fac.createBilinearGroup();
