45c19ed1fe8044eb9da7661d678b0021d4de164a
rheitjoh <rheitjoh@github.com>
2020-07-04 13:54:30 +0200

    Update javadoc of some visitor classes for handling tree access structures

--- src/main/java/de/upb/crypto/craco/abe/accessStructure/util/Visitor.java
+++ src/main/java/de/upb/crypto/craco/abe/accessStructure/util/Visitor.java
@@ -3,47 +3,46 @@ package de.upb.crypto.craco.abe.accessStructure.util;
 import de.upb.crypto.craco.abe.accessStructure.exception.WrongAccessStructureException;
 
 /**
- * This is a implementation of the visitor performed on the threshold tree
- * (created out of the extended boolean formula)
+ * Interface specifying the methods any visitor for a threshold tree should implement
+ * (created out of the extended boolean formula).
  *
- * @param <F> Type of the returning value of the function
- *            <code>getResultOfCurrentNode</code>
+ * @param <F> type of the return value of the function
+ *            {@link Visitor#getResultOfCurrentNode()}
  * @author pschleiter
  */
 public interface Visitor<F> {
 
     /**
-     * Returns the value, that is eventually calculated during the method visit.
+     * Returns the value that was calculated during {@link Visitor#visit(TreeNode)}.
      *
-     * @return
      * @throws WrongAccessStructureException
      */
-    public F getResultOfCurrentNode() throws WrongAccessStructureException;
+    F getResultOfCurrentNode() throws WrongAccessStructureException;
 
     /**
-     * return a visitor for the next child. Then this child will be called with
-     * this visitor.
+     * Returns a visitor for the next child. The resulting visitor is functionally the same, but any state that was
+     * mutated while visiting the current child may be reset.
      *
      * @return a visitor of the same kind
      * @throws WrongAccessStructureException
      */
-    public Visitor<F> getVisitorForNextChild()
+    Visitor<F> getVisitorForNextChild()
             throws WrongAccessStructureException;
 
     /**
-     * Insert the result of the child of current node, so that the current
+     * Inserts the result of the child of current node, so that the current
      * visitor can calculate out of it, its own value.
      *
      * @param input
      */
-    public void putResultOfChild(F input);
+    void putResultOfChild(F input);
 
     /**
-     * method, that will performed on <code>currentNode</code>
+     * Performs some kind of computation on the given <code>currentNode</code>.
      *
-     * @param currentNode node of the threshold tree
+     * @param currentNode node of the threshold tree to visit
      * @throws WrongAccessStructureException
      */
-    public void visit(TreeNode currentNode)
+    void visit(TreeNode currentNode)
             throws WrongAccessStructureException;
 }
