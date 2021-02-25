5629f1a2dd9d742c290176fb7ce690c2878c98cb
Jan Bobolz <jan.bobolz@uni-paderborn.de>
2021-02-25 21:48:35 +0100

    Fixed stupid error

--- src/test/java/org/cryptimeleon/craco/ser/standalone/params/AccumulatorParams.java
+++ src/test/java/org/cryptimeleon/craco/ser/standalone/params/AccumulatorParams.java
@@ -1,17 +1,17 @@
 package org.cryptimeleon.craco.ser.standalone.params;
 
-import com.sun.tools.javac.util.List;
 import org.cryptimeleon.craco.accumulator.nguyen.NguyenAccumulatorScheme;
 import org.cryptimeleon.craco.ser.standalone.StandaloneTestParams;
 import org.cryptimeleon.math.structures.groups.counting.CountingBilinearGroup;
 import org.cryptimeleon.math.structures.groups.elliptic.BilinearGroup;
 
+import java.util.Arrays;
 import java.util.Collection;
 
 public class AccumulatorParams {
 
     public static Collection<StandaloneTestParams> get() {
-        return List.of(
+        return Arrays.asList(
                 new StandaloneTestParams(NguyenAccumulatorScheme.setup(new CountingBilinearGroup(128, BilinearGroup.Type.TYPE_3), 3))
         );
     }
