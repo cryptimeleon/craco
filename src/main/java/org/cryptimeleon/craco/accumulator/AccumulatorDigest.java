705877f7f4224a1d27ea7dc830fe6fb73d132d7b
Jan Bobolz <jan.bobolz@uni-paderborn.de>
2021-02-25 21:40:22 +0100

    Reworked Accumulator interfaces and implementation

---
+++ src/main/java/org/cryptimeleon/craco/accumulator/AccumulatorDigest.java
@@ -0,0 +1,9 @@
+package org.cryptimeleon.craco.accumulator;
+
+import org.cryptimeleon.math.serialization.Representable;
+
+/**
+ * A short representation of a set.
+ */
+public interface AccumulatorDigest extends Representable {
+}
