6894b6ba05ce611cf886c5319901931281c24c2b
Hanna <hsiek@mail.uni-paderborn.de>
2022-02-11 10:52:00 +0100

    started AKOT15 trapdoor commitment scheme

---
+++ src/main/java/org/cryptimeleon/craco/commitment/TrapdoorValue.java
@@ -0,0 +1,10 @@
+package org.cryptimeleon.craco.commitment;
+
+import org.cryptimeleon.math.hash.UniqueByteRepresentable;
+import org.cryptimeleon.math.serialization.Representable;
+
+/**
+ * Trapdoor to a commitment.
+ */
+public interface TrapdoorValue extends Representable, UniqueByteRepresentable {
+}
