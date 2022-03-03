a291909aa16b505de9d822a1f9f90c40ce42525c
Hanna <hsiek@mail.uni-paderborn.de>
2022-03-03 10:37:18 +0100

    added message check to TC

--- src/main/java/org/cryptimeleon/craco/sig/sps/akot15/tc/TCAKOT15CommitmentScheme.java
+++ src/main/java/org/cryptimeleon/craco/sig/sps/akot15/tc/TCAKOT15CommitmentScheme.java
@@ -6,6 +6,7 @@ import org.cryptimeleon.craco.common.plaintexts.MessageBlock;
 import org.cryptimeleon.craco.common.plaintexts.PlainText;
 import org.cryptimeleon.craco.common.plaintexts.RingElementPlainText;
 import org.cryptimeleon.craco.sig.SignatureKeyPair;
+import org.cryptimeleon.craco.sig.sps.SPSMessageSpaceVerifier;
 import org.cryptimeleon.craco.sig.sps.akot15.AKOT15SharedPublicParameters;
 import org.cryptimeleon.craco.sig.sps.akot15.pos.*;
 import org.cryptimeleon.craco.sig.sps.akot15.tcgamma.*;
@@ -34,37 +35,47 @@ import org.cryptimeleon.math.structures.rings.zn.Zp.ZpElement;
  * https://eprint.iacr.org/2015/076.pdf
  *
  */
-public class TCAKOT15CommitmentScheme implements CommitmentScheme {
+public class TCAKOT15CommitmentScheme implements CommitmentScheme, SPSMessageSpaceVerifier {
 
+    /**
+     * The public parameters used by this scheme
+     */
     @Represented
     AKOT15SharedPublicParameters pp;
 
+    /**
+     * An instance of {@link SPSPOSSignatureScheme} used for commitment calculation
+     */
     @Represented
     SPSPOSSignatureScheme posInstance;
 
+    /**
+     * An instance of {@link TCGAKOT15CommitmentScheme} used for commitment calculation
+     */
     @Represented
     TCGAKOT15CommitmentScheme gbcInstance;
 
+    /**
+     * In order to match the {@link CommitmentScheme} interface, the scheme stores its own keys
+     * instead of the key passed as a parameter of commit() / verify().
+     */
     @Represented
     TCGAKOT15CommitmentKey commitmentKey;
 
-    @Represented(restorer = "")
+    /**
+     * Instead of running several instances of {@link SPSPOSSignatureScheme}, the scheme calculates its own one-time
+     *      keys and passes each of them to a single {@code posInstance} in sequence.
+     */
+    @Represented(restorer = "[Zp]")
     ZpElement[] oneTimeSecretKeys;
 
-    @Represented(restorer = "")
+    /**
+     * Instead of running several instances of {@link SPSPOSSignatureScheme}, the scheme calculates its own one-time
+     *      keys and passes each of them to a single {@code posInstance} in sequence.
+     */
+    @Represented(restorer = "[Zp]")
     GroupElement[] oneTimePublicKeys;
 
-    MessageBlock commitMsg;
-
-    //TODO set these up better
-
-    private GroupElement getG1GroupGenerator() {
-        return (pp instanceof SPSXSIGPublicParameters) ? ((SPSXSIGPublicParameters)pp).getGroup1ElementF1() : pp.getG1GroupGenerator();
-    }
-
-    private GroupElement getG2GroupGenerator() {
-        return (pp instanceof SPSXSIGPublicParameters) ? ((SPSXSIGPublicParameters)pp).getGroup2ElementF1() : pp.getG2GroupGenerator();
-    }
 
     public TCAKOT15CommitmentScheme(AKOT15SharedPublicParameters pp) {
         super();
@@ -102,14 +113,20 @@ public class TCAKOT15CommitmentScheme implements CommitmentScheme {
             throw new IllegalArgumentException("This is not a valid commitment key for this scheme");
         }
 
-        if(!(plainText instanceof MessageBlock)) {
-            throw new IllegalArgumentException("This is not a valid message for this scheme");
+        //if plainText contains only a single group element, wrap it in a MessageBlock
+        if((plainText instanceof GroupElementPlainText)) {
+            plainText = new MessageBlock(plainText);
         }
+
+        // check if the plainText matches the structure expected by the scheme. It should be a MessageBlock composed
+        //      of GroupElements in G_2
+        doMessageChecks(plainText, pp.getMessageLength(), pp.getG2GroupGenerator().getStructure());
 
         MessageBlock messageBlock = (MessageBlock) plainText;
         TCGAKOT15CommitmentKey ck = (TCGAKOT15CommitmentKey) commitmentKey;
 
-        SignatureKeyPair<SPSPOSVerificationKey, SPSPOSSigningKey> posKeyPair = (SignatureKeyPair<SPSPOSVerificationKey, SPSPOSSigningKey>) posInstance.generateKeyPair();
+        SignatureKeyPair<SPSPOSVerificationKey, SPSPOSSigningKey> posKeyPair =
+                (SignatureKeyPair<SPSPOSVerificationKey, SPSPOSSigningKey>) posInstance.generateKeyPair();
         generateOneTimeKeys();
 
         SPSPOSSignature[] sigmas = new SPSPOSSignature[pp.getMessageLength()];
@@ -127,9 +144,8 @@ public class TCAKOT15CommitmentScheme implements CommitmentScheme {
             msg_com[i] = new RingElementPlainText(oneTimeSecretKeys[i - 2]);
         }
 
-        commitMsg = new MessageBlock(msg_com);
-
-        CommitmentPair gbcCommitmentPair = gbcInstance.commit(commitMsg);
+        // commit using TC_gamma
+        CommitmentPair gbcCommitmentPair = gbcInstance.commit(new MessageBlock(msg_com));
 
         TCAKOT15OpenValue open = new TCAKOT15OpenValue(
                 ((TCGAKOT15OpenValue)gbcCommitmentPair.getOpenValue()).getGroup1ElementR(),
@@ -141,14 +157,19 @@ public class TCAKOT15CommitmentScheme implements CommitmentScheme {
         return new CommitmentPair(gbcCommitmentPair.getCommitment(), open);
     }
 
-
+    /**
+     * Instead of running several instances of {@link SPSPOSSignatureScheme}, the scheme calculates its own one-time
+     *      keys and passes each of them to a single {@code posInstance} in sequence.
+     *
+     * The individual one-time keys are generated the same way as in {@link SPSPOSSignatureScheme}.
+     */
     private void generateOneTimeKeys() {
         oneTimeSecretKeys = new ZpElement[pp.getMessageLength()];
         oneTimePublicKeys = new GroupElement[pp.getMessageLength()];
 
         for (int i = 0; i < oneTimePublicKeys.length; i++) {
             oneTimeSecretKeys[i] = pp.getZp().getUniformlyRandomNonzeroElement();
-            oneTimePublicKeys[i] = getG1GroupGenerator().pow(oneTimeSecretKeys[i]).compute();
+            oneTimePublicKeys[i] = pp.getG1GroupGenerator().pow(oneTimeSecretKeys[i]).compute();
         }
     }
 
@@ -171,18 +192,25 @@ public class TCAKOT15CommitmentScheme implements CommitmentScheme {
             throw new IllegalArgumentException("This is not a valid commitment for this scheme");
         }
 
-        if(!(plainText instanceof MessageBlock)) {
-            throw new IllegalArgumentException("This is not a valid message for this scheme");
+        //if plainText contains only a single group element, wrap it in a MessageBlock
+        if((plainText instanceof GroupElementPlainText)) {
+            plainText = new MessageBlock(plainText);
         }
 
+        // check if the plainText matches the structure expected by the scheme. It should be a MessageBlock composed
+        //      of GroupElements in G_2
+        doMessageChecks(plainText, pp.getMessageLength(), pp.getG2GroupGenerator().getStructure());
+
         MessageBlock messageBlock = (MessageBlock) plainText;
-        TCGAKOT15CommitmentKey ck = (TCGAKOT15CommitmentKey) commitmentKey;
         TCGAKOT15Commitment com = (TCGAKOT15Commitment) commitment;
         TCAKOT15OpenValue open = (TCAKOT15OpenValue) openValue;
 
         for (int i = 0; i < messageBlock.length(); i++) {
 
-            if( !posInstance.verify(new MessageBlock(messageBlock.get(i)), open.getSpsPosSignatures()[i], open.spsPosVerificationKey, oneTimePublicKeys[i])) {
+            if( !posInstance.verify(new MessageBlock(
+                    messageBlock.get(i)),
+                    open.getSpsPosSignatures()[i],
+                    open.spsPosVerificationKey, oneTimePublicKeys[i])) {
                 return false;
             }
         }
@@ -206,10 +234,10 @@ public class TCAKOT15CommitmentScheme implements CommitmentScheme {
 
         GroupElementPlainText[] msgBlock = new GroupElementPlainText[pp.getMessageLength()];
         msgBlock[0] = new GroupElementPlainText(
-                getG2GroupGenerator().pow(pp.getZp().injectiveValueOf(bytes))
+                pp.getG2GroupGenerator().pow(pp.getZp().injectiveValueOf(bytes))
         );
         for (int i = 1; i < pp.getMessageLength(); i++) {
-            msgBlock[i] = new GroupElementPlainText(getG2GroupGenerator());
+            msgBlock[i] = new GroupElementPlainText(pp.getG2GroupGenerator());
         }
 
         return new MessageBlock(msgBlock);
@@ -222,14 +250,14 @@ public class TCAKOT15CommitmentScheme implements CommitmentScheme {
 
     @Override
     public Commitment restoreCommitment(Representation repr) {
-        return new TCGAKOT15Commitment(getG2GroupGenerator().getStructure(), repr);
+        return new TCGAKOT15Commitment(pp.getG2GroupGenerator().getStructure(), repr);
     }
 
     @Override
     public OpenValue restoreOpenValue(Representation repr) {
         return new TCAKOT15OpenValue(
-                getG1GroupGenerator().getStructure(),
-                getG2GroupGenerator().getStructure(),
+                pp.getG1GroupGenerator().getStructure(),
+                pp.getG2GroupGenerator().getStructure(),
                 repr);
     }
 
