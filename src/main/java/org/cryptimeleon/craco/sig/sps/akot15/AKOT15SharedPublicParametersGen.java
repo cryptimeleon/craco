293dd1204c42c13209c34514bbc080d77a7660d9
Hanna <hsiek@mail.uni-paderborn.de>
2022-03-03 09:50:48 +0100

    removed superfluous parameter generator

--- src/main/java/org/cryptimeleon/craco/sig/sps/akot15/AKOT15SharedPublicParametersGen.java
+++ src/main/java/org/cryptimeleon/craco/sig/sps/akot15/AKOT15SharedPublicParametersGen.java
@@ -5,6 +5,13 @@ import org.cryptimeleon.math.structures.groups.debug.DebugBilinearGroup;
 import org.cryptimeleon.math.structures.groups.elliptic.BilinearGroup;
 import org.cryptimeleon.math.structures.groups.elliptic.type3.bn.BarretoNaehrigBilinearGroup;
 
+/**
+ * generates a set of public parameters to be used by the schemes:
+ * {@link org.cryptimeleon.craco.sig.sps.akot15.pos.SPSPOSSignatureScheme},
+ * {@link org.cryptimeleon.craco.sig.sps.akot15.tc.TCAKOT15CommitmentScheme},
+ * {@link org.cryptimeleon.craco.sig.sps.akot15.tcgamma.TCGAKOT15CommitmentScheme}
+ *
+ */
 public class AKOT15SharedPublicParametersGen {
 
     public static AKOT15SharedPublicParameters generateParameters(int securityParameter, int numberOfMessages, boolean debugMode) {
